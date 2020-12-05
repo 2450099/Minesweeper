d6a8271630358b2e7475894197cc6b482b606e16
Connor Koefelda <2450099@lfschools.net>
2020-12-05 22:25:39 +0000

    Undo test

--- Game.java
+++ Game.java
@@ -16,150 +16,160 @@
 class Game 
 {
     // These are the commands that are available.
-    private final String INITIAL_COMMANDS = "go quit help back";
-    private final String INITIAL_COMMANDS = "go quit help back items pick inventory drop exits";
+    private final String INITIAL_COMMANDS = "go quit help";
     // This is the current room that that player is in
     private Room currentRoom;
-    //This is the room the player was just in
-@@ -25,9 +25,11 @@
+    // This is the list of items that the person is carrying
     private String inventory;
     // tells if the game is over or not
     private boolean finished;
-    // used to get rid of an itme in a room
-    String tempItem = "";
     // These are references to the rooms that are available.  
     // The actual rooms are created in createRooms().
     private Room outside, lab, store, gym, office;  
-    private Room outside, lab, store, gym, office, parking;  
 
     // This is the parser that deals with input.  You should not have to touch this.
     private Parser parser;
-@@ -50,7 +52,7 @@ public Game()
+    // picture that is being shown
+    private Image image;
+
+    /**
+     *  Create and play the game
+     */
+    public static void main(String args[]) 
+    {
+        Game game = new Game();
+        game.play();
+    }
+
+    /**
+     * Game constructor
+     */
+    public Game() 
     {
         createRooms();
         parser = new Parser(INITIAL_COMMANDS);
         inventory = "";
-        inventory = " ";
         finished = false;
         image = new Image("images/lfhs.jpg");
     }
-@@ -66,13 +68,18 @@ private void createRooms()
+
+    /**
+     * Create all the rooms and link their exits together.
+     */
+    private void createRooms()
+    {
+        // create the rooms
+        outside = new Room("outside the building");
+        lab = new Room("the lab");
         store = new Room("the store");
         gym = new Room("the gym");
         office = new Room("the main office");
-        parking = new Room("the parking lot");
 
         // initialise room exits
         outside.setExits(null, lab, gym, store);
-        // room.setExits(N,E,S,w)
-        outside.setExits(parking, lab, gym, store);
-        outside.setItems(" dog ");
         lab.setExits(null, null, null, outside);
         store.setExits(null, outside, null, null);
-        store.setItems(" money bottle ");
         gym.setExits(outside, office, null, null);
         office.setExits(null, null, null, gym);
-        parking.setExits(null,null,outside,null);
 
         currentRoom = outside;  // start game outside
     }
-@@ -122,13 +129,22 @@ private void processCommand(Command command)
+
+    /**
+     *  Main play routine.  Loops until end of play.
+     */
+    public void play() 
+    {            
+        printWelcome();
+
+        // Enter the main command loop.  Here we repeatedly read commands and
+        // execute them until the game is over.
+        while (!finished)
+        {
+            Command command = parser.getCommand();
+            processCommand(command);
+        }
+        System.out.println("Thank you for playing.  Goodbye.");
+    }
+
+    /**
+     * Print out the opening message for the player.
+     */
+    private void printWelcome()
+    {
+        System.out.println();
+        System.out.println("Welcome to Adventure!");
+        System.out.println("Adventure is a new, incredibly boring adventure game.");
+        System.out.println("Type 'help' if you need help.");
+        System.out.println();
+        System.out.println(currentRoom);
+    }
+
+    /**
+     * Given a command, process the command.
+     * If this command ends the game, true is returned, otherwise false is returned.
+     */
+    private void processCommand(Command command) 
+    {
+        if(command.isUnknown())
+        {
+            System.out.println("I don't know what you mean...");
+            return;
+        }
+
         String commandWord = command.getCommandWord();
         if (commandWord.equals("help")) {
             printHelp();
         } else if (commandWord.equals("go"))
         {
-        } else if (commandWord.equals("go")) {
             goRoom(command);
         } else if (commandWord.equals("quit")) {
             finished = true;  // signal that we want to quit
-        } else if (commandWord.equals("back")) {
-          goBack();
-        } else if (commandWord.equals("items")) {
-          printItems();
-        } else if (commandWord.equals("pick")) {
-          pickItem(command);
-        } else if (commandWord.equals("inventory")) {
-          printInventory();
-        } else if (commandWord.equals("drop")) {
-          dropItem(command);
-        } else if (commandWord.equals("exits")) {
-          System.out.println(currentRoom);
         } else {
             System.out.println("Command not implemented yet.");
         }
-@@ -155,6 +171,7 @@ private void printHelp()
+
+    }
+
+    /**
+     * Print out some help information.
+     * Here we print a cryptic message and a list of the 
+     * command words.  Maybe you can improve on this.
+     */
+    private void printHelp() 
+    {
+        System.out.println("You are lost. You are alone. You wander");
+        System.out.println("around at Lake Forest High School.");
+        System.out.println();
         System.out.println("Your command words are:");
         parser.showCommands();
     }
 
-
     /** 
      * Try to go to one direction. If there is an exit, enter the new
-@@ -184,4 +201,66 @@ private void goRoom(Command command)
+     * room, otherwise print an error message.
+     */
+    private void goRoom(Command command) 
+    {
+        if(!command.hasSecondWord())
+        {
+            // if there is no second word, we don't know where to go...
+            System.out.println("Go where?");
+            return;
+        }
+
+        String direction = command.getSecondWord();
+
+        // See what the next room in that direction is
+        Room nextRoom = currentRoom.nextRoom(direction);
+
+        if (nextRoom == null) // there is no room in that direction
+            System.out.println("There is no door!");
+        else // go into the next room
+        {
+            // we just entered a new room 
+            currentRoom = nextRoom;
             System.out.println(currentRoom);
         }
-    }
-    /**
-    *this moves an item from the room to your inventory
-    */
-    private void pickItem(Command command) {
-      if (!command.hasSecondWord()) {
-        // if there is no second word, we don't know what to pick up...
-        System.out.println("Pick up what?");
-        return;
-      }
-      String item = command.getSecondWord();
-      //the code within the if statement is to ensure the game doesn't crash or act oddly if you input something that isn't an item in the room
-      if (currentRoom.getItems().indexOf(item) > -1 && currentRoom.getItems().substring(currentRoom.getItems().indexOf(item) - 1, currentRoom.getItems().indexOf(item)).equals(" ") && currentRoom.getItems().substring(currentRoom.getItems().indexOf(item) + item.length(), currentRoom.getItems().indexOf(item) + item.length() + 1).equals(" ")) {
-        tempItem = currentRoom.getItems();
-        tempItem = tempItem.substring(0,tempItem.indexOf(item)) + tempItem.substring(tempItem.indexOf(item)+item.length());
-        currentRoom.setItems(tempItem);
-        inventory += item + " ";
-        System.out.println("Picked up " + item);
-      } else {
-        System.out.println("That is not an item");
-      }
-    }
-    /**
-    *this lists the items in your inventory
-    */
-    public void printInventory() {
-      if (inventory.trim().equals("")) {
-        System.out.println("You have nothing in your inventory!");
-      } else {
-        System.out.println(inventory);
-      }
-    }
-    /**
-    *this moves an item from your inventory to the room
-    */
-    public void dropItem(Command command) {
-      if (!command.hasSecondWord()) {
-        // if there is no second word, we don't know what to pick up...
-        System.out.println("Drop what?");
-        return;
-      }
-      String item = command.getSecondWord();
-        //the code within the if statement is to ensure the game doesn't crash or act oddly if you input something that isn't an item you have
-      if (inventory.indexOf(item) > -1 && inventory.substring(inventory.indexOf(item) - 1, inventory.indexOf(item)).equals(" ") && inventory.substring(inventory.indexOf(item) + item.length(), inventory.indexOf(item) + item.length() + 1).equals(" ")) {
-        tempItem = inventory;
-        tempItem = tempItem.substring(0,tempItem.indexOf(item)) + tempItem.substring(tempItem.indexOf(item)+item.length());
-        inventory = tempItem;
-        currentRoom.setItems(currentRoom.getItems() + item + " ");
-        System.out.println("Dropped " + item);
-      } else {
-        System.out.println("That is not an item");
-      }
-    }
-    /**
-    *this lists the items currently in the room
-    */
-    public void printItems() {
-      if (currentRoom.getItems().trim().equals("")) {
-        System.out.println("There are no items in this room!");
-      } else {
-        System.out.println(currentRoom.getItems());
-      }
     }
 }
